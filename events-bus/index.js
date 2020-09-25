585302d47feb84ed4968fb82cfd10420bec132d3
parkoon <devparkoon@gmail.com>
2020-09-25 15:51:46 +0900

    feat: implement event bus server 🔌

---
+++ events-bus/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Server is running on 4005");
+});
