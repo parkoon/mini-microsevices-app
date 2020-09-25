d61299fc8d0625125f2207b5f2df0afba9b1704e
parkoon <devparkoon@gmail.com>
2020-09-25 15:57:20 +0900

    feat: send event to event bus server ✉️

--- comments/index.js
+++ comments/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 
 const app = express();
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -22,6 +23,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
